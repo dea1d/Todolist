1019b9f0d899bcb32818e8c6edb8c662bcf82a50
dea1d <yura.shennikov05@mail.ru>
2019-04-12 16:00:13 +0300

    Первое изменение

--- js/main.js
+++ js/main.js
@@ -44,11 +44,11 @@ new Vue({
       var mm = today.getMonth()+1; //January is 0!
       var yyyy = today.getFullYear();
 
-      if(dd<10) {
+      if (dd < 10) {
           dd = '0'+dd
       }
 
-      if(mm<10) {
+      if (mm < 10) {
           mm = '0'+mm
       }
 
